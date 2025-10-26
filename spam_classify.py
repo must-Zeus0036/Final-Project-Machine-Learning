d0af1f0e8f04b2c0d7f84d9fdb1403202d765846
must-Zeus0036 <mustafa.al-bayati0036@stud.hkr.se>
2025-10-26 18:24:49 +0100

    debug logic in LR and checking the Flask framework working prefectly. add some picture showing our UI

--- spam_classify.py
+++ spam_classify.py
@@ -6,7 +6,43 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 
-# Download NLTK data
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+# Class LogisticRegressionScratch moved here so pickle can load the trained model without errors
+class LogisticRegressionScratch:
+    def __init__(self, lr=0.001, n_iters=1000):
+        self.lr = lr
+        self.n_iters = n_iters
+        self.weights = None
+        self.bias = None
+
+    def fit(self, X, y):
+        if hasattr(X, "toarray"):
+            X = X.toarray()
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        for _ in range(self.n_iters):
+            linear_pred = np.dot(X, self.weights) + self.bias
+            predictions = sigmoid(linear_pred)
+
+            dw = (1 / n_samples) * np.dot(X.T, (predictions - y))
+            db = (1 / n_samples) * np.sum(predictions - y)
+
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
+
+    def predict(self, X):
+        if hasattr(X, "toarray"):
+            X = X.toarray()
+        linear_pred = np.dot(X, self.weights) + self.bias
+        y_pred = sigmoid(linear_pred)
+        return [1 if i > 0.5 else 0 for i in y_pred]
+
+
 # 'punkt' is needed for word_tokenize.
 # 'stopwords' is needed for the list of English stop words.
 nltk.download("punkt")
@@ -15,18 +51,14 @@ nltk.download("stopwords")
 
 # Load the pre-trained Random Forest model and the TF-IDF vectorizer.
 with open("rf_model.pkl", "rb") as model_file:
-    rf_model = pickle.load(model_file)
-
+    lr_model = pickle.load(model_file)
 with open("tfidf_vectorizer.pkl", "rb") as model_file:
     tfidf_vectorizer = pickle.load(model_file)
-    
 
+        
 # Define preprocessing function (Must match training pipeline)
 def preprocess_text(text):
-    """
-    Applies the exact same cleaning, tokenization, stop word removal,
-    and stemming steps from model
-    """
+    
     text = text.lower() #Convert to lowercase
     
     text = re.sub(r'<[^>]*>', '', text) #Remove HTML tags
@@ -57,7 +89,7 @@ def predict():
     vectorized = tfidf_vectorizer.transform([preprocessed_text])
     
     #Make the prediction using the Random Forest model rf_model
-    prediction = rf_model.predict(vectorized)[0]
+    prediction = lr_model.predict(vectorized)[0]
     
     #Convert the numerical prediction (ham 0 or spam 1)
     label = "spam" if prediction == 1 else "ham"
